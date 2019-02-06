c10cf879b5ddd5ff9c903d46026dd5efb044ff4e
C.  Keijzer <c.keijzer@live.nl>
2019-02-06 14:36:54 +0100

    add authentication

--- database/factories/ShareFactory.php
+++ database/factories/ShareFactory.php
@@ -4,6 +4,12 @@ use Faker\Generator as Faker;
 
 $factory->define(App\Share::class, function (Faker $faker) {
     return [
-        //
+        //------------------------------------------------------------------------------------
+        'share_name' => $faker->realText(rand(5, 20)),
+        'share_price' => $faker->rand(100,9999),
+        'share_qty' => $faker->rand(1,99999),,
+
+
+        //------------------------------------------------------------------------------------
     ];
 });
