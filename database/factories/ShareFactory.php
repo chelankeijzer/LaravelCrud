9bb731e794bbbe40658bdd4b6b80dd43f8a63eb4
C.  Keijzer <c.keijzer@live.nl>
2019-02-06 15:12:35 +0100

    add authentication

--- database/factories/ShareFactory.php
+++ database/factories/ShareFactory.php
@@ -5,9 +5,9 @@ use Faker\Generator as Faker;
 $factory->define(App\Share::class, function (Faker $faker) {
     return [
         //------------------------------------------------------------------------------------
-        'share_name' => $faker->realText(rand(5, 20)),
-        'share_price' => $faker->rand(100,9999),
-        'share_qty' => $faker->rand(1,99999),,
+        'share_name' => $faker->share_name,
+        'share_price' => $faker->share_price,
+        'share_qty' => $faker->share_qty
 
 
         //------------------------------------------------------------------------------------
--- routes/web.php
+++ routes/web.php
@@ -18,3 +18,7 @@ Route::get('/', function () {
 Route::resource('shares', 'ShareController');
 
 
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
